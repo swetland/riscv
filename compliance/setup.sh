911bfb87f7a8b5c9560a96cb4aad24d928a64053
Brian Swetland <swetland@frotz.net>
2019-10-23 10:51:52 -0700

    compliance: scripts to checkout and run riscv-compliance

---
+++ compliance/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -e
+
+git clone git@github.com:riscv/riscv-compliance.git rvc
+
+mkdir -p rvc/riscv-target/rvsim/device/rv32i
+
+ln -s ../../../compliance_io.h rvc/riscv-target/rvsim/compliance_io.h
+ln -s ../../../compliance_test.h rvc/riscv-target/rvsim/compliance_test.h
+ln -s ../../../../../makefile.inc rvc/riscv-target/rvsim/device/rv32i/Makefile.include
+
